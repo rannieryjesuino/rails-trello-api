d03fcd6d0ad9b8b3c9d4ce759aeedcf0affe1b1d
Ranniery Jesuino <ranniery.jesuino@gmail.com>
2018-06-15 23:22:20 -0300

    Adicionado verificadao de board ao mudar a atividade
    Agora nao eh mais possivel mudar a atividade (card) para uma lista que esteja em outra board

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,77 +1,86 @@
 class Api::V1::CardsController < Api::V1::ApiController
-    
+
     before_action :set_card, only: [:show, :update, :destroy]
-    
+
     def index
-     
+
         @cards = Card.all
-        
+
         render json: @cards
-        
+
     end
-    
+
     # GET /api/v1/cards/1
-    
+
     def show
-    
+
      render json: @card
-    
+
     end
-    
+
     # POST /api/v1/cards
-    
+
     def create
-    
+
         @card = Card.new card_params
-        
+
         if @card.save
-        
+
             render json: @card, status: :created
-        
+
         else
-        
+
             render json: @card.errors, status: :unprocessable_entity
-        
+
         end
-    
+
     end
-    
+
     # PATCH/PUT /api/v1/cards/1
-    
+
     def update
-    
-        if @card.update card_params
-        
-            render json: @card
-        
-        else
-        
-            render json: @card.errors, status: :unprocessable_entity
-        
+
+        if board_diferente
+          render json: {errors: "Não é possível mudar a atividade para um quadro diferente!"}
+          return
         end
-    
+
+        if @card.update card_params
+
+            render json: @card
+
+        else
+
+            render json: @card.errors, status: :unprocessable_entity
+
+        end
+
     end
-    
+
     # DELETE /api/v1/cards/1
-    
+
     def destroy
-    
+
         @card.destroy
-    
+
     end
-    
+
     private
 
         def set_card
-        
-             @card = Card.find(params[:id])
-        
+
+           @card = Card.find(params[:id])
+
         end
 
         def card_params
-        
+
             params.require(:card).permit(:name, :desc, :list_id)
-        
+
+        end
+
+        def board_diferente
+            @card.list.board != List.find(card_params[:list_id]).board ? true : false
         end
 
 end
